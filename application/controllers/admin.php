e335d44dc39c4d268ccc55966b08474b1e6c9754
Candra Dwi Prasetyo <candradwiprasetyo@Candras-MacBook-Pro.local>
2016-04-26 11:55:27 +0700

    new

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -29,18 +29,63 @@ class Admin extends CI_Controller {
 				$data_user  = $result;
 			}
 			
-			if($this->session->userdata('param1')){
+			// Number of applications
+			if($this->session->userdata('param1_value')){
 				$param1_value = $this->session->userdata('param1_value');
 				$param1_type = $this->session->userdata('param1_type');
 			}else{
 				$param1_value = date("Y-m-d")."_".date("Y-m-d");
 				$param1_type = "Today";
 			}
-
 			$data['total_application'] = $this->admin_model->get_total_application($param1_value);
+
+			// Top positions
+			if($this->session->userdata('param2_value')){
+				$param2_value = $this->session->userdata('param2_value');
+				$param2_type = $this->session->userdata('param2_type');
+			}else{
+				$param2_value = date("Y-m-d")."_".date("Y-m-d");
+				$param2_type = "Today";
+			}
+			$data['top_position'] = $this->admin_model->get_top_position($param2_value);
+
+			// top education
+			if($this->session->userdata('param3_value')){
+				$param3_value = $this->session->userdata('param3_value');
+				$param3_type = $this->session->userdata('param3_type');
+			}else{
+				$param3_value = date("Y-m-d")."_".date("Y-m-d");
+				$param3_type = "Today";
+			}
+			$data['top_education'] = $this->admin_model->get_top_education($param3_value);
+
+			// top city
+			if($this->session->userdata('param4_value')){
+				$param4_value = $this->session->userdata('param4_value');
+				$param4_type = $this->session->userdata('param4_type');
+			}else{
+				$param4_value = date("Y-m-d")."_".date("Y-m-d");
+				$param4_type = "Today";
+			}
+			$data['top_city'] = $this->admin_model->get_top_city($param4_value);
+
+			// top 10 position
+			$list_top_ten_position =  $this->admin_model->get_top_ten_position();
+
+			// top 10 education
+			$list_top_ten_education =  $this->admin_model->get_top_ten_education();
+
+			// top 10 city
+			$list_top_ten_city =  $this->admin_model->get_top_ten_city();
+			
 			
 			$this->load->view('admin_layout/header', array( 'data_head' => $data_head, 'data_user' => $data_user));
-			$this->load->view('admin_layout/dashboard', array('data' => $data));
+			$this->load->view('admin_layout/dashboard', 
+				array('data' => $data, 
+						'list_top_ten_position' => $list_top_ten_position,
+						'list_top_ten_education' => $list_top_ten_education,
+						'list_top_ten_city' => $list_top_ten_city
+					));
 			$this->load->view('admin_layout/footer'); 
 
  	}
@@ -50,6 +95,21 @@ class Admin extends CI_Controller {
 			if($param == 1){
 				$this->session->set_userdata('param1_value', $value);
 				$this->session->set_userdata('param1_type', $type);
+			}
+
+			if($param == 2){
+				$this->session->set_userdata('param2_value', $value);
+				$this->session->set_userdata('param2_type', $type);
+			}
+
+			if($param == 3){
+				$this->session->set_userdata('param3_value', $value);
+				$this->session->set_userdata('param3_type', $type);
+			}
+
+			if($param == 4){
+				$this->session->set_userdata('param4_value', $value);
+				$this->session->set_userdata('param4_type', $type);
 			}
 			
 
--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -20,6 +20,7 @@ class Admin_model extends CI_Model{
 	
 	function get_total_application($param)
 	{
+
 		$param = explode("_", $param);
 		$date1 = $param[0];
 		$date2 = $param[1];
@@ -27,7 +28,7 @@ class Admin_model extends CI_Model{
 				FROM basic_infos
 				WHERE basic_info_date >= '$date1' AND basic_info_date <= '$date2'; 
 				";
-
+				
 		$query = $this->db->query($sql);
 		
 		$result = null;
@@ -37,7 +38,92 @@ class Admin_model extends CI_Model{
 		return $result['result'];
 	}
 
-	public function get_format_param1_type($data){
+	function get_top_position($param)
+	{
+
+		$param = explode("_", $param);
+		$date1 = $param[0];
+		$date2 = $param[1];
+		$sql = "SELECT a.position_id, a.position_name, jumlah
+								FROM positions a
+								JOIN (
+								
+									SELECT count( a.basic_info_id ) AS jumlah, portfolio_position_id
+									FROM basic_infos a
+									JOIN portfolios b on b.basic_info_id = a.basic_info_id
+									WHERE basic_info_date >= '$date1' AND basic_info_date <= '$date2'
+									GROUP BY b.portfolio_position_id
+								) AS b ON b.portfolio_position_id = a.position_id
+								order by jumlah desc, position_id asc
+								limit 1; 
+				";
+				
+		$query = $this->db->query($sql);
+		
+		$result = null;
+		foreach ($query->result_array() as $row) $result = ($row);
+		
+		$result['position_name'] = ($result['position_name']) ? $result['position_name'] : '-';
+		return $result['position_name'];
+	}
+
+	function get_top_education($param)
+	{
+
+		$param = explode("_", $param);
+		$date1 = $param[0];
+		$date2 = $param[1];
+		$sql = "SELECT a.education_type_name AS result, jumlah
+								FROM education_types a
+								JOIN (
+								
+									SELECT count( a.basic_info_id ) AS jumlah, basic_info_last_education_type_id
+									FROM basic_infos a
+									WHERE basic_info_date >= '$date1' AND basic_info_date <= '$date2'
+									GROUP BY a.basic_info_last_education_type_id
+								) AS b ON b.basic_info_last_education_type_id = a.education_type_id
+								order by jumlah desc, education_type_id asc
+								limit 1; 
+				";
+				
+		$query = $this->db->query($sql);
+		
+		$result = null;
+		foreach ($query->result_array() as $row) $result = ($row);
+		
+		$result['result'] = ($result['result']) ? $result['result'] : '-';
+		return $result['result'];
+	}
+
+	function get_top_city($param)
+	{
+
+		$param = explode("_", $param);
+		$date1 = $param[0];
+		$date2 = $param[1];
+		$sql = "SELECT a.city_name AS result, jumlah
+								FROM city a
+								JOIN (
+								
+									SELECT count( a.basic_info_id ) AS jumlah, basic_info_city_id
+									FROM basic_infos a
+									WHERE basic_info_date >= '$date1' AND basic_info_date <= '$date2'
+									GROUP BY a.basic_info_city_id
+								) AS b ON b.basic_info_city_id = a.city_id
+								order by jumlah desc, city_id asc
+								limit 1; 
+				";
+				
+		$query = $this->db->query($sql);
+		
+		$result = null;
+		foreach ($query->result_array() as $row) $result = ($row);
+		
+		$result['result'] = ($result['result']) ? $result['result'] : '-';
+		return $result['result'];
+	}
+
+	public function get_format_param_type($data){
  		$data = str_replace("%20", " ", $data);
  		if($data == 'Apply'){
  			$data = 'Custom Range';
@@ -45,5 +131,89 @@ class Admin_model extends CI_Model{
  		return $data;
 
  	}
+
+ 	function get_total_application_bydate($date)
+	{
+
+		$sql = "SELECT count( a.basic_info_id ) AS result, basic_info_city_id
+									FROM basic_infos a
+									WHERE basic_info_date = '$date'	
+				";
+				
+		$query = $this->db->query($sql);
+		
+		$result = null;
+		foreach ($query->result_array() as $row) $result = ($row);
+		
+		$result['result'] = ($result['result']) ? $result['result'] : '0';
+		return $result['result'];
+	}
+
+	function get_top_ten_position()
+	{
+
+		$query = "SELECT a.position_id, a.position_name, jumlah
+								FROM positions a
+								LEFT JOIN (
+								
+									SELECT count( a.basic_info_id ) AS jumlah, portfolio_position_id
+									FROM basic_infos a
+									JOIN portfolios b on b.basic_info_id = a.basic_info_id
+									GROUP BY b.portfolio_position_id
+								) AS b ON b.portfolio_position_id = a.position_id
+								order by jumlah desc, position_id asc
+								limit 10";
+        $query = $this->db->query($query);
+       // query();
+        if ($query->num_rows() == 0)
+            return array();
+        $data = $query->result_array();
+        foreach ($data as $index => $row) {}
+        return $data;
+	}
+
+	function get_top_ten_education()
+	{
+
+		$query = "SELECT a.education_type_name, jumlah
+								FROM education_types a
+								JOIN (
+								
+									SELECT count( a.basic_info_id ) AS jumlah, basic_info_last_education_type_id
+									FROM basic_infos a
+									GROUP BY a.basic_info_last_education_type_id
+								) AS b ON b.basic_info_last_education_type_id = a.education_type_id
+								order by jumlah desc, education_type_id asc
+								limit 10";
+        $query = $this->db->query($query);
+       // query();
+        if ($query->num_rows() == 0)
+            return array();
+        $data = $query->result_array();
+        foreach ($data as $index => $row) {}
+        return $data;
+	}
+
+	function get_top_ten_city()
+	{
+
+		$query = "SELECT a.city_name, jumlah
+								FROM city a
+								JOIN (
+								
+									SELECT count( a.basic_info_id ) AS jumlah, basic_info_city_id
+									FROM basic_infos a
+									GROUP BY a.basic_info_city_id
+								) AS b ON b.basic_info_city_id = a.city_id
+								order by jumlah desc, city_id asc
+								limit 10";
+        $query = $this->db->query($query);
+       // query();
+        if ($query->num_rows() == 0)
+            return array();
+        $data = $query->result_array();
+        foreach ($data as $index => $row) {}
+        return $data;
+	}
 	
 }
