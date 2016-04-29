780321b1e6194a5c9ddcbff214bc1b15d3bb5ee2
Candra Dwi Prasetyo <candradwiprasetyo@Candras-MacBook-Pro.local>
2016-04-29 18:32:09 +0700

    new

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
-	
+
 	function __construct(){
 		parent::__construct();
 		$this->load->model('admin_model');
@@ -9,26 +9,26 @@ class Admin extends CI_Controller {
 		$this->load->library('access');
 
 		$this->session->set_userdata('menu_active', 5);
-		
+
 		$logged_admin = $this->session->userdata('logged_admin');
 		if($logged_admin == ""){
 			redirect('login_admin');
 		}
-		
-		
+
+
 	}
- 	
+
 	public function index() {
 
 			$data_head['title'] = "Dashboard";
-			
+
 			$data_user = array();
 			$result = $this->admin_model->read_id($this->session->userdata('user_id'));
-				
+
 			if($result){
 				$data_user  = $result;
 			}
-			
+
 			// Number of applications
 			if($this->session->userdata('param1_value')){
 				$param1_value = $this->session->userdata('param1_value');
@@ -77,19 +77,19 @@ class Admin extends CI_Controller {
 
 			// top 10 city
 			$list_top_ten_city =  $this->admin_model->get_top_ten_city();
-			
-			
+
+
 			$this->load->view('admin_layout/header', array( 'data_head' => $data_head, 'data_user' => $data_user));
-			$this->load->view('admin_layout/dashboard', 
-				array('data' => $data, 
+			$this->load->view('admin_layout/dashboard',
+				array('data' => $data,
 						'list_top_ten_position' => $list_top_ten_position,
 						'list_top_ten_education' => $list_top_ten_education,
 						'list_top_ten_city' => $list_top_ten_city
 					));
-			$this->load->view('admin_layout/footer'); 
+			$this->load->view('admin_layout/footer');
 
  	}
-	
+
 	public function search($param, $value, $type) {
 
 			if($param == 1){
@@ -111,13 +111,16 @@ class Admin extends CI_Controller {
 				$this->session->set_userdata('param4_value', $value);
 				$this->session->set_userdata('param4_type', $type);
 			}
-			
+
 
 			redirect('admin');
 
  	}
 
- 	
-	
-	
+	public function logout(){
+		$this->session->sess_destroy();
+		redirect('login_admin');
+	}
+
+
 }
